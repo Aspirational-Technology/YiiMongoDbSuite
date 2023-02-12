d1efc89c411bde2172091cb2dff74ca0df073256
worldtravelerr <sfarcacha@outlook.com>
2023-02-12 18:47:53 -0500

    Update

---
+++ vendor/composer/installed.php
@@ -0,0 +1,34 @@
+<?php return array(
+    'root' => array(
+        'name' => 'aspirational-technology/yiimongodbsuite',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '3a6b154f0fb78ec489273d1bd461d1d7f780d9d6',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'aspirational-technology/yiimongodbsuite' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '3a6b154f0fb78ec489273d1bd461d1d7f780d9d6',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'roave/security-advisories' => array(
+            'pretty_version' => 'dev-latest',
+            'version' => 'dev-latest',
+            'reference' => 'c0e29643df152bd7fbe3de189f6c2c16c7d4c2a2',
+            'type' => 'metapackage',
+            'install_path' => NULL,
+            'aliases' => array(
+                0 => '9999999-dev',
+            ),
+            'dev_requirement' => true,
+        ),
+    ),
+);
